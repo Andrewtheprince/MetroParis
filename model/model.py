82f670d04f8f7cb5f5ce6e92fdff3e4e4275af68
Giuseppe Averta <g.averta3@gmail.com>
2025-04-30 14:25:38 +0200

    Lezione del 30/04/2025

--- model/model.py
+++ model/model.py
@@ -12,6 +12,65 @@ class Model:
         for f in self._fermate:
             self._idMapFermate[f.id_fermata] = f
 
+    def getBFSNodesFromTree(self, source):
+        tree = nx.bfs_tree(self._grafo, source)
+        archi = list(tree.edges())
+        nodi = list(tree.nodes())
+        return nodi[1:]
+
+    def getDFSNodesFromTree(self, source):
+        tree = nx.dfs_tree(self._grafo, source)
+        nodi = list(tree.nodes())
+        return nodi[1:]
+
+    def getBFSNodesFromEdges(self, source):
+        archi = nx.bfs_edges(self._grafo, source)
+        res = []
+        for u, v in archi:
+            res.append(v)
+        return res
+
+    def getDFSNodesFromEdges(self, source):
+        archi = nx.dfs_edges(self._grafo, source)
+        res = []
+        for u, v in archi:
+            res.append(v)
+        return res
+
+    def buildGraphPesato(self):
+        self._grafo.clear()
+        self._grafo.add_nodes_from(self._fermate)
+        self.addEdgesPesatiV2()
+
+    def addEdgesPesati(self):
+        self._grafo.clear_edges()
+        allEdges = DAO.getAllEdges()
+        for edge in allEdges:
+            u = self._idMapFermate[edge.id_stazP]
+            v = self._idMapFermate[edge.id_stazA]
+
+            if self._grafo.has_edge(u, v):
+                self._grafo[u][v]["weight"] += 1
+            else:
+                self._grafo.add_edge(u, v, weight=1)
+
+    def addEdgesPesatiV2(self):
+        self._grafo.clear_edges()
+        allEdgesPesati = DAO.getAllEdgesPesati()
+
+        for e in allEdgesPesati:
+            self._grafo.add_edge(
+                self._idMapFermate[e[0]],
+                self._idMapFermate[e[1]],
+                weight = e[2]
+            )
+    def getArchiPesoMaggiore(self):
+        edges = self._grafo.edges(data=True)
+        res = []
+        for e in edges:
+            if self._grafo.get_edge_data(e[0],e[1])["weight"]>1:
+                res.append(e)
+        return(res)
     def buildGraph(self):
         # Aggiungiamo i nodi
         self._grafo.add_nodes_from(self._fermate)
@@ -31,7 +90,7 @@ class Model:
         tic = datetime.now()
         self.addEdges3()
         toc = datetime.now()
-        print("Tempo modo 3:", (toc-tic))
+        print("Tempo modo 3:", (toc - tic))
 
     def addEdges1(self):
         """Aggiungo gli archi con doppio ciclo sui nodi,
